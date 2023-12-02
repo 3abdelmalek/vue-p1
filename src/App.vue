397d5523376ce93deef87c4b60ac74fd259736a8
abdelmalek33 <115729556+abdelmalek33@users.noreply.github.com>
2023-12-02 23:34:05 +0100

    First vue-project this just experience

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<!-- App.vue -->
+
+<script>
+// Import the Hello component from './components/Hello.vue'
+import Hello from './components/Hello.vue';
+
+// Export the App component
+export default {
+    name: 'App', // Component name
+    components: {
+        Hello // Register the Hello component for use in this component
+    }
+}
+</script>
+
+<template>
+<!-- Use the Hello component within the template -->
+<Hello />
+</template>
+
+<style>
+/* Your styles go here */
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,24 @@
+<!-- components/Hello.vue -->
+
+<script>
+// The script section of the Hello component
+export default {
+    name: 'Hello', // Component name
+    data() {
+        // Data function returns the initial data for the component
+        return {
+            msg: 'Hello ',
+            name: 'Luca'
+        };
+    }
+}
+</script>
+
+<template>
+<!-- The template section contains the HTML structure of the component -->
+<div> {{ msg + name }} </div>
+</template>
+
+<style>
+/* Styles specific to the Hello component go here */
+</style>
