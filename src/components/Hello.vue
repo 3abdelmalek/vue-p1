30c62674037c5353aadea50bab9d70c1c4329bdf
abdelmalek33 <115729556+abdelmalek33@users.noreply.github.com>
2023-12-06 20:12:29 +0100

    new someThings #2

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,40 +1,26 @@
 <template>
-<div class="container">
+<div :class="[`container bg-${theme} p-2 w-100`]">
     <nav :class="[`bg-${theme}`, `navbar-${theme}`]" class="navbar navbar-expand-lg">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Meat-Store</a>
             <div>
-            <button @click.prevent="changeTheme" type="button" :class="[`text-${nextTheme()}`, `border-${nextTheme()}`]" class="btn mx-1">
+                <!-- To Change Background Of NavBar -->
+                <button @click.prevent="changeTheme" type="button" :class="[`text-${nextTheme()}`, `border-${nextTheme()}`]" class="btn mx-1">
                     {{ nextTheme().charAt(0).toUpperCase() + nextTheme().substring(1) }}
-            </button>
+                </button>
 
-
-
-
-
-
-
-                
                 <button class=" mx-1 navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
             </div>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- To Print All Page In NavBar From Array Of Objects -->
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Meat
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Normal Meat</a></li>
-                            <li><a class="dropdown-item" href="#">Monster Meat</a></li>
-                            <li><a class="dropdown-item" href="#">Mini Meat</a></li>
-                        </ul>
+                    <li v-for="(navbarAtt, index) in navbarAtts" class="nav-item" :key="index">
+                        <a class="nav-link" aria-current="page" :href="navbarAtt.lnk">{{ navbarAtt.name }}</a>
                     </li>
                 </ul>
+
                 <form class="d-flex">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                     <button :class="[`btn btn-outline-${nextTheme()}`]" type="submit">Search</button>
@@ -64,15 +50,18 @@
             <span class="visually-hidden"></span>
         </button>
     </div>
+
     <div class="input-group flex-nowrap my-2">
-        <span class="input-group-text" id="addon-wrapping">@</span>
-        <input v-model="name" type="text" class="form-control" placeholder="name" aria-label="Username" aria-describedby="addon-wrapping">
+        <span class="input-group-text" id="addon-wrapping">@page</span>
+        <input v-model="name" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping">
     </div>
+
     <div class="input-group mb-3">
         <span class="input-group-text" id="basic-addon3">example.com</span>
-        <input v-model="email" type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+        <input v-model="lnk" type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
     </div>
-    <button @click="showMsg" class="btn btn-outline-primary px-4 " type="submit" value="Submit"> Send </button>
+    <!-- TO Add New Page To NavBar and Show Funny Msg -->
+    <button @click="() => { showMsg(); addPage();}" class="btn btn-outline-primary px-4 " type="submit" value="Submit"> Send </button>
 </div>
 </template>
 
@@ -82,25 +71,39 @@ export default {
     data() {
         return {
             name: '',
-            email: '',
+            lnk: '',
             theme: 'light',
-            navbarAtt: []
+            navbarAtts: [{
+                    name: 'Home',
+                    lnk: 'Home.com'
+                },
+                {
+                    name: 'Meat',
+                    lnk: 'Meat.com'
+                },
+            ],
         }
     },
     methods: {
+        addPage() {
+            this.navbarAtts.push({
+                name: this.name,
+                lnk: this.lnk
+            });
+        },
         nextTheme() {
             return this.theme == 'light' ? 'dark' : 'light';
         },
         changeTheme() {
             let theme = 'light'
-            if(this.theme == 'light'){
+            if (this.theme == 'light') {
                 theme = 'dark'
             }
             this.theme = theme
         },
         showMsg() {
-            if(this.userName && this.email ) {
-                alert("Did You Want Some Monster Meat (#4) :)")
+            if (this.name && this.lnk) {
+                alert(`Did You Want Some Monster Meat (🍖🍖🍖🍖) 😂 Dr: " ${this.name} "`)
             }
         }
     },
